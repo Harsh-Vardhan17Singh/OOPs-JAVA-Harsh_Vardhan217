6c0a656fe8464f90b11afeaf26b655018add798b
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-11-01 15:03:57 +0530

    Searching String of java

---
+++ Searching_String.java
@@ -0,0 +1,10 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        String s = "Harsh";
+        System.out.println(s.indexOf('H')); 
+        System.out.println(s.lastIndexOf('h')); 
+        System.out.println(s.contains("ar"));
+    }
+}
