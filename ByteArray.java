626ba77d653a7d6ff173b06eeed418f04493fadf
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-11-01 14:34:20 +0530

    Printing My Name In ASCII value
    Created a byte Array in which i stored my  ASCII Values Of my name

---
+++ ByteArray.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Geeks {
+    public static void main(String[] args) {
+        
+        byte[] byteArray = { 72 , 65 , 82 , 83 , 72 };
+        String str3 = new String(byteArray);
+        System.out.println("My Name Using ASCII Value: " + str3);
+    }
+}
