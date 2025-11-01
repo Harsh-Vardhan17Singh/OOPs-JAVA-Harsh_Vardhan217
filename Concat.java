063e50786d3d727b1a347c166afa96deab023784
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-11-01 14:45:03 +0530

    Concatination in java

---
+++ Concat.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Geeks {
+    public static void main(String[] args) {
+        
+       String s1 = "Harsh";
+       String s2 = "Vardhan";
+       String s3 = s1+" "+s2;
+       String s4 = s1.concat(" "+s2);
+       
+       System.out.println(s3);
+       
+    }
+}
