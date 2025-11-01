4089e33f8ef3827f77462b8a20afbe2ca47ca4c9
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-11-01 15:29:03 +0530

    Substring in Java

---
+++ SubString.java
@@ -0,0 +1,6 @@
+
+class Main {
+    public static void main(String[] args) {
+        String s = "HarshVardhan";
+        System.out.println(s.substring(3)); 
+        System.out.println(s.substring(4)); 
